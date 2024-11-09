1c6df6ce6e3734c24f6cd06d8a3df310fc76b101
Jakub Soliński <solinski.jakub@gmail.com>
2024-11-09 20:54:53 +0100

    Fix tests

--- backend/app/tests/crud/test_orders.py
+++ backend/app/tests/crud/test_orders.py
@@ -1,9 +1,21 @@
-from fastapi.encoders import jsonable_encoder
+from backend.app.tests.utils.user import create_random_user
+from backend.app.tests.utils.utils import random_lower_string, random_oso_kod
 from sqlmodel import Session
+from app.crud import order as crud
 
 from app import create_order
 from app.models import OrderCreate
 
 
+
 def test_create_order(db: Session) -> None:
-    pass
+    user = create_random_user(db)
+    owner_id = user.id
+    assert owner_id is not None
+    kh_kod = random_oso_kod()
+    description = random_lower_string()
+    order_in = OrderCreate(kh_kod=kh_kod, description=description)
+    order = crud.create_order(session=db, order_in=order_in, owner_id=owner_id)
+    assert order.kh_kod == kh_kod
+    assert order.description == description
+    assert order.owner_id == owner_id
--- backend/app/tests/utils/order.py
+++ backend/app/tests/utils/order.py
@@ -1,7 +1,7 @@
-from app.crud import user as crud
+from app.crud import order as crud
 from app.models import Order, OrderCreate
 from app.tests.utils.user import create_random_user
-from app.tests.utils.utils import random_lower_string
+from app.tests.utils.utils import random_lower_string, random_oso_kod
 from sqlmodel import Session
 
 
@@ -9,7 +9,7 @@ def create_random_order(db: Session) -> Order:
     user = create_random_user(db)
     owner_id = user.id
     assert owner_id is not None
-    title = random_lower_string()
+    kh_kod = random_oso_kod()
     description = random_lower_string()
-    order_in = OrderCreate(title=title, description=description)
+    order_in = OrderCreate(kh_kod=kh_kod, description=description)
     return crud.create_order(session=db, order_in=order_in, owner_id=owner_id)
