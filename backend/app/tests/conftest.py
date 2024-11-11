f38323154ed7ef12fe4cbcf90ab6185c2a40f238
Jakub Soliński <solinski.jakub@gmail.com>
2024-11-11 10:17:13 +0100

    Don't remove default user

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -19,7 +19,7 @@ def db() -> Generator[Session, None, None]:
         yield session
         statement = delete(Order)
         session.execute(statement)
-        statement = delete(User)
+        statement = delete(User).where(User.email != settings.FIRST_SUPERUSER)
         session.execute(statement)
         session.commit()
 
